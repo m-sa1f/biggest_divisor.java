4d1c2039763a3d70aa69eefed9eafedbf03d1155
m-sa1f <141922923+m-sa1f@users.noreply.github.com>
2023-08-10 16:53:10 +0530

    biggest_divisor.java

--- biggest_divisor.java
+++ biggest_divisor.java
@@ -5,52 +5,6 @@ public class biggest_divisor {
         Scanner scan = new Scanner(System.in);
         // For no. of inputs
         int n = scan.nextInt();
-
-
-        // int f = scan.nextInt();
-        // int s = scan.nextInt();
-
-        // int divisor = 0;
-        // int i = 1;
-        // int sum = Math.max(f,s);
-        // while(i<sum){
-
-        //     if(f%i==0 && s%i==0){
-        //         divisor = i;
-        //     }
-        //     i++;
-        // }
-        // System.out.println(divisor);
-
-
-        // int i = 1;
-        // while(i<=n){
-        //     int a = scan.nextInt();
-        //     int j = 1;
-        //     while(j<=a){
-        //         System.out.print("*");
-        //         j++;
-        //     }
-        //     System.out.println();
-        //     i++;
-        // }
-        // int i = 1;
-        // while(i<=n){
-        //     int j = n;
-        //     while(j>i){
-        //         System.out.print(j);
-        //         j--;
-        //     }
-        //     System.out.print("*");
-        //     j = 1;
-        //     while(j<i){
-        //         System.out.print(j);
-        //         j++;
-        //     }
-        //     System.out.println();
-        //     i++;
-        // }
-
         if (n<=2){
             System.out.println("-1");
         }
@@ -74,5 +28,5 @@ public class biggest_divisor {
     // cout<<var / 2;
     System.out.print(var/2);
   }
-    }
+ }
 }
